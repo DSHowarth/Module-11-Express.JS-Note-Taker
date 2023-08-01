50d0ba8341b4b9aea83be68c16d5cb37c052b2c4
Dash <dkshowarth@gmail.com>
2023-08-01 15:42:09 -0700

    add POST route to api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,22 @@
 const apiRoute = require('express').Router();
 const fs = require('fs');
 
-apiRoute.get('/notes', (req, res) => {
+apiRoute.get('/', (req, res) => {
 
     fs.readFile('../db/db.json', 'utf8', (data) => {
         res.send(data);
+    })
+})
+
+apiRoute.post('/', (req, res) => {
+    
+
+    fs.readFile('../db/db.json', 'utf8', (data) => {
+        console.log(req);
+        const notesDB = JSON.parse(data);
+        notesDB.append(req.body);
+        fs.writeFile('../db/db.json', JSON.stringify(notesDB), (err) => {
+            err ? console.log(err) : console.log('Added note to DB');
+        });
     })
 })
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // redirect api requests to api.js
-app.use('/api', api);
+app.use('/api/notes', api);
 
 // Allow access to public css/js, landing page index.html
 app.use(express.static('public'));
